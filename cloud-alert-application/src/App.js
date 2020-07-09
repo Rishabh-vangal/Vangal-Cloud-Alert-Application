3881a136e0927fbfe0ac8068d7a19ab827bedb40
Rishabh Jain <rishabh101.99@gmail.com>
2020-07-09 03:05:14 -0700

    Added sign in with Amazon
    Not fully functional yet

--- cloud-alert-application/src/App.js
+++ cloud-alert-application/src/App.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import LoginScreen from './components/LoginScreen.js';
+// const {google} = require('googleapis');
+// const cloudbilling = google.cloudbilling('v1');
 
 class App extends React.Component {
   constructor(props) {
@@ -12,7 +14,41 @@ class App extends React.Component {
       service: ''
     }
 
-    this.clientID_AWS = '';
+    // async function main () {
+    //   const authClient = await authorize();
+    //   const request = {
+    //     // Required. The resource name of the billing account associated with the projects that
+    //     // you want to list. For example, `billingAccounts/012345-567890-ABCDEF`.
+    //     name: 'billingAccounts/my-billing-account',  // TODO: Update placeholder value.
+    
+    //     auth: authClient,
+    //   };
+    
+    //   let response;
+    //   do {
+    //     if (response && response.nextPageToken) {
+    //       request.pageToken = response.nextPageToken;
+    //     }
+    //     response = (await cloudbilling.billingAccounts.projects.list(request)).data;
+    //     const projectBillingInfoPage = response.projectBillingInfo;
+    //     if (projectBillingInfoPage) {
+    //       for (let i = 0; i < projectBillingInfoPage.length; i++) {
+    //         // TODO: Change code below to process each resource in `projectBillingInfoPage`:
+    //         console.log(JSON.stringify(projectBillingInfoPage[i], null, 2));
+    //       }
+    //     }
+    //   } while (response.nextPageToken);
+    // }
+    // main();
+    
+    // async function authorize() {
+    //   const auth = new google.auth.GoogleAuth({
+    //     scopes: ['https://www.googleapis.com/auth/cloud-platform']
+    //   });
+    //   return await auth.getClient();
+    // }
+
+    this.clientID_AWS = 'amzn1.application-oa2-client.58f592dc27c9454ca8c44ef43de17bb1';
     this.clientID_Azure = '1854d86b-8e91-4db9-846e-0da965c041d6';
     this.clientID_Google = '825269243215-cblac6ek5fl7virhp4ci0puhgvakr83c.apps.googleusercontent.com';
     this.onSignIn = this.onSignIn.bind(this);
--- cloud-alert-application/src/components/LoginScreen.js
+++ cloud-alert-application/src/components/LoginScreen.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import MicrosoftLogin from "react-microsoft-login";
 import GoogleLogin from 'react-google-login';
+import { withAuthenticator, AmplifySignOut, AmplifyAmazonButton } from '@aws-amplify/ui-react'
 
 function LoginScreen(props) {
 
   const onSignInAzure = (err, data) => {
+    console.log(data)   
     let email = data.authResponseWithAccessToken.account.userName;
     let name = data.authResponseWithAccessToken.account.name;
     let loggedIn = err == null;
@@ -14,7 +16,6 @@ function LoginScreen(props) {
       name: name,
       email: email
     })
-    console.log(data.authResponseWithAccessToken)   
     props.onSignIn(state);
   };
 
@@ -23,13 +24,21 @@ function LoginScreen(props) {
   return (
     <div>
       <h1>Vangal Cloud Alert Application</h1>
+        <AmplifyAmazonButton 
+          clientId={props.clientID_AWS}
+          buttonId="Sign in with Google"
+          onSuccess={onSignInAzure}
+          onFailure={onSignInAzure}
+          authCallback={onSignInAzure}
+        />
+        <br/>
         <MicrosoftLogin 
           clientId={props.clientID_Azure} 
           authCallback={onSignInAzure}
           />
-        <br />
+        <br/>
         <GoogleLogin 
-          clientId={props.clientID_Google} 
+          clientId={props.clientID_AWS} 
           buttonId="Sign in with Google"
           onSuccess={props.onSignInGoogle}
           onFailure={props.onSignInGoogle}
