bd6ae0f73616888ca450695a7a61a704826505d5
Rishabh Jain <rishabh101.99@gmail.com>
2020-07-23 15:27:16 -0700

    Storing log of each Microsoft sign in

--- cloud-alert-application/backend/server.js
+++ cloud-alert-application/backend/server.js
@@ -2,7 +2,49 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 
+let AzureData;
+
 app.use(cors());
+
+app.get('/Azure/GetCredentialLog', (req, res) => {
+    res.send(AzureData);
+});
+
+app.get('/AWS', (req, res) => {
+    let accountId = 'AE2RW6W34NPM4S7MZ6XDHY3OXUQQ'; // amzn1.account.AE2RW6W34NPM4S7MZ6XDHY3OXUQQ
+    let key = 'qwer';
+    let secret = 'qwer';
+    let bucket = 'company-aws-billing';
+    let region = 'us-west-2';
+
+    let billing = require('aws-billing')(accountId, key, secret, bucket, region);
+
+    billing(function (err, costs) {
+      console.log('err', err);
+      console.log('costs', costs);
+    });
+
+
+    let config = { 
+        apiVersion: '2017-10-25',
+        accessKeyId : 'amzn1.application-oa2-client.58f592dc27c9454ca8c44ef43de17bb1',
+        secretAccessKey : 'b5a51a9fa71de7e654643719fe64b2a944ca09415b4cea39a3ed3a9719f6ca82',    // Atza|IwEBIAKNpwyduT7xGnVyAmrzTa4JEjz4EIQRXcsejXL3MvEKBbX9KQndY5yaPAriZgKkqAdicMyAQb_lkTzdGLbW2tyKUSrGJM0HEIKz6scPWRKonH3LKZPZcugxKy-S4eVR3P7-sQHrRi3Ly_j7_x2M7ih6HWIecLjE4ec7DnP-KLuVlBAk4yFvNEEmehlevuI_OwFtb5xABogvxsQbLcFJ3sPL30DUivvjC0sU65yLOle1awn_3p0s3mJzTQSSV9ZNqgfJrqp10FOrn34sCyUki6ZzR9n-so0NuAtXJTfdoEWb-uyknIR-g4fRQtVV7KESs5PL5QXLYeACWsUGZiN1iYA2xvmUHvpSjn3m6wS9J1aswnW63A5EVeePDpOC0aJzn2EPc_rSlYz-ls-6Ks7UeLs2EjkhNL2RU1K5Lj7yTZxllA
+        region : 'us-east-1'
+    }
+    
+    let CostExplorer = require('aws-cost-explorer');
+    let ce = CostExplorer(config);
+    
+    ce.getMonthToDateCosts(null, function(err, data) {
+        if (err) {
+            console.log(err);
+        } else {
+            console.dir(data, { depth: null });
+        }
+    });
+
+    res.send('success');
+});
 
 let azureSignInCode;
 
@@ -17,12 +59,20 @@ app.get('/Azure/SignIn', (req, res) => {
     console.logs = [];
   
     var msRestAzure = require("ms-rest-azure")
+    const BillingManagement = require('azure-arm-billing');
     msRestAzure.interactiveLogin(function(err, credentials) {
         if (err) 
         {
             return console.log(err);
         }
+        AzureData.push(credentials)
         res.send(credentials);
+
+        let client = new BillingManagement(credentials, '0909e1a6-c169-4174-a202-386547f9047e');
+        client.invoices.list().then((invoices) => {
+            console.log('List of invoices: ');
+            console.log(invoices, {depth: null, colors: true});
+        });
     });
 
     setTimeout(function() {
@@ -34,6 +84,8 @@ app.get('/Azure/SignIn', (req, res) => {
 });
 
 app.listen(8080, function() {
+    AzureData = [];
+
     console.stdlog = console.log.bind(console);
     console.logs = [];
     console.log = function(){
--- cloud-alert-application/src/components/LoginScreen.js
+++ cloud-alert-application/src/components/LoginScreen.js
@@ -7,7 +7,7 @@ import axios from 'axios';
 
 function LoginScreen(props) {
   const onSignInMicrosoft = () => {
-    axios.get('/Azure/SignIn').then(response => {
+    axios.get('http://167.172.124.9:8080/Azure/SignIn').then(response => {
       console.log(response.data);
       console.log(response.data.tokenCache._entries);
       console.log(response.data.tokenCache._entries[1].accessToken);
@@ -34,7 +34,7 @@ function LoginScreen(props) {
         .catch(error => console.log(error));     
     });
 
-    axios.get('/Azure/SignInCode')
+    axios.get('http://167.172.124.9:8080/Azure/SignInCode')
       .then(response => {
         console.log(response.data);
         alert('Enter this code in the popup window to sign in: ' + response.data.Code);
@@ -45,39 +45,6 @@ function LoginScreen(props) {
 
     
   }
-
-  const onSignInAzure = (err, data) => {
-    console.log(data)   
-    let email = data.authResponseWithAccessToken.account.userName;
-    let name = data.authResponseWithAccessToken.account.name;
-    let loggedIn = err == null;
-    
-    const requestOptions = {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/x-www-form-urlencoded;charset=UTF-8',
-          'Accept': 'application/json'
-        },
-        mode: 'no-cors',
-        body: 'client_id=1854d86b-8e91-4db9-846e-0da965c041d6&scope=https://graph.microsoft.com/.default&client_secret=A8-q.GIe4_.J89_-cKglpduI9_jPJ-ijwj&grant_type=client_credentials'
-    };
-    console.log(requestOptions);
-    fetch('https://login.microsoftonline.com/common/oauth2/v2.0/token', requestOptions)
-      .then(response => console.log(response))
-      .catch(error => console.log(error));
-
-
-    const state = ({
-      loggedIn: loggedIn,
-      error: err,
-      name: name,
-      email: email,
-      service: 'Azure',
-      data: data,
-      bearerToken: ''
-    })
-    props.onSignIn(state);
-  };
 
   const onSignInAmazon = (data) => {
     console.log(data);
